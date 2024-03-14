aceba98b0f6d0a32d409e865347f1e9e211be739
aganyo360 <wilsonaganyo@gmail.com>
2024-03-14 20:03:34 +0300

    removing an item from list

---
+++ exercise_list.py
@@ -0,0 +1,8 @@
+invitees = ['Nyakiti', 'Elizabeth', 'Phillip', 'Dinna']
+message= 'Brice invites you, '
+print(message, invitees[0])
+print(message, invitees[1])
+print(message, invitees[2])
+print(message, invitees[3])
+
+print(f'unforutnaly {invitees.pop(-3)} will not be available')
