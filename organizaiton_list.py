b23bc9aa975960cb8779cc9a2148266e4df19dd3
aganyo360 <wilsonaganyo@gmail.com>
2024-03-15 20:05:19 +0300

    sorting a list using the sort method

---
+++ organizaiton_list.py
@@ -0,0 +1,11 @@
+# sorting a list permananently with the sort method
+cars =[
+    'toyota', 'mazda', 'vitz', 'mercedes', 'premio', 'ferari', 'audi'
+]
+cars.sort()
+# the aboe cars.sort sorts the list in ascending order
+print(cars)
+
+cars.sort(reverse=True)
+# reverse= trues sorts the list in descending order 
+print(cars)
