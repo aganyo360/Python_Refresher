012e197c20acab83c92cfe7e17391e1a0db7998a
aganyo360 <wilsonaganyo@gmail.com>
2024-03-16 15:09:40 +0300

    regular expression search  method

--- REGEX/regerx.py
+++ REGEX/regerx.py
@@ -1,7 +1,13 @@
 import re
 #  the Regular has so many inbuild expresions
 
-# 1. Match Function  re.match(pattern, string)  check pattern at the beginning of the targeted string
+# 1. Match Function  re.match(pattern, string)  check pattern at the beginning of the targeted string else returns none
 string = 'I am learning fundamental of data engineering'
 matching = re.match('I', string)
 print(matching)
+
+# 2. Search function
+#  if the match is available then it returns the atch 
+#  if first occurance it will return not considering the second occurance
+searching = re.search('a', string)
+print(f'searching', searching)
