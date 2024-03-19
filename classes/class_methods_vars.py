200abeceddded6a49f234a679611ca324744f433
aganyo360 <wilsonaganyo@gmail.com>
2024-03-19 19:29:58 +0300

    using third parameter in range function

--- classes/class_methods_vars.py
+++ classes/class_methods_vars.py
@@ -11,3 +11,8 @@ print(numbers)
 #when we introduce a third argument in range it acts a skipping value
 even_numbers = list(range(2,21,2))
 print(f'The even numbers are:',even_numbers)
+
+# the below code fragment prints odd numbers in form of lists
+
+odd_numbers = list(range(1,21,2))
+print(f'The odd numbers are:',odd_numbers)
