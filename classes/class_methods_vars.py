8c0d23e66b9fcdf3345b28312b336d098fab24c7
aganyo360 <wilsonaganyo@gmail.com>
2024-03-19 19:28:35 +0300

    using third parameter in range function

--- classes/class_methods_vars.py
+++ classes/class_methods_vars.py
@@ -6,3 +6,8 @@ for name in names:
 
 numbers =list(range(1,7)) 
 print(numbers)
+
+
+#when we introduce a third argument in range it acts a skipping value
+even_numbers = list(range(2,21,2))
+print(f''even_numbers)
