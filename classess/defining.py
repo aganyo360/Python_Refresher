74d5a6b2dada13d6984790b64b024cde022e0506
aganyo360 <wilsonaganyo@gmail.com>
2024-04-26 08:35:17 +0300

    grouped class files into a single folder

--- classess/defining.py
+++ classess/defining.py
@@ -3,3 +3,9 @@ class Dog():
         self.name = name
         self.age = age
         self.gender = gender
+
+    def sit(self):
+        print(self.name.title() + " is now sitting.")
+    
+    def roll_over(self):
+        print(self.name.title() + " rolled over!")
