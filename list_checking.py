f6158d2cb5de5bba4a033d244b5819609197a0a6
aganyo360 <wilsonaganyo@gmail.com>
2024-04-26 08:01:15 +0300

    checking item in list

--- list_checking.py
+++ list_checking.py
@@ -1,2 +1,7 @@
 # we are going to check whether an item is not in a list
-
+# create a list with 5 elements
+names_list = ['Peter', 'James', 'John', 'Matthew', 'Thomas']
+if 'Peter' in names_list:
+    print('Peter is in the list')
+else:
+    print('Peter is not in the list')
