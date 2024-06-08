d7416b5157735a15f08cf2ab82b346abdf2f1baf
aganyo360 <wilsonaganyo@gmail.com>
2024-06-08 18:11:25 +0300

    OOP in py

---
+++ REGEX/encapsulation.py/enca.p.py
@@ -0,0 +1,24 @@
+class Encapsulation:
+    def __init__(self,a):
+        self.a = a
+    def public_var(self,b):
+        self.b=b
+    def protected_var(self,c):
+        self._c=c
+    def private_var(self,d):
+        self.__d=d
+        #  the __ double undersocre show private and _c single underscore show protected variable
+e = Encapsulation(50)
+# the following are the forms of visibility in python
+e.public_var(60)
+e.protected_var(70)
+e.private_var(900)
+
+print('Accessing my public_varibalbe',e.b)
+
+print('Accessing my protected_varibalbe',e._c)
+
+
+print('Accessing my private_varibalbe',e.__d)
+print('Accessing my private_varibalbe',e.__d)
+
