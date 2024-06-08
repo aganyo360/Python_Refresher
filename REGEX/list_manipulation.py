ef924a027acd62e88df2e9e81eb88780d89af40b
aganyo360 <wilsonaganyo@gmail.com>
2024-06-08 18:00:52 +0300

    list

---
+++ REGEX/list_manipulation.py
@@ -0,0 +1,13 @@
+# assume we have a list of cities
+cities = []
+
+cities.append('nakuru')
+cities.append('Kisumu')
+cities.append('nairobi')
+cities.append('naivasha')
+
+print(cities)
+
+# to delete an item from a list use the del list_name[index]
+del cities[0]
+print(cities)
