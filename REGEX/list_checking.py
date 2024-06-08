1e1459b15bcc7b4a6a9a00da0311b304d542d171
aganyo360 <wilsonaganyo@gmail.com>
2024-06-08 18:00:21 +0300

    list in py

---
+++ REGEX/list_checking.py
@@ -0,0 +1,7 @@
+# we are going to check whether an item is not in a list
+# create a list with 5 elements
+names_list = ['Peter', 'James', 'John', 'Matthew', 'Thomas']
+if 'Amelia' not in names_list:
+    print('Amelia not in the list')
+else:
+    print('Amelia is in list')
