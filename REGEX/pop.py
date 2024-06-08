8cf71075bcd34380ca9fab6e440fa3c5dd30ee23
aganyo360 <wilsonaganyo@gmail.com>
2024-06-08 18:02:06 +0300

    ds

---
+++ REGEX/pop.py
@@ -0,0 +1,19 @@
+# using the pop method
+# the pop method let's you work with the removed element, 
+# it considers the list as the stack and removes the last index of the list
+cities=[]
+cities.append('nakuru')
+cities.append('Kisumu')
+cities.append('nairobi')
+cities.append('naivasha')
+print(cities)
+
+# now introducing the pop method
+popped_city = cities.pop()
+print(cities)
+print(popped_city)
+
+
+# we can pop any element from any position 
+second_city = cities.pop(1)
+print(cities)
