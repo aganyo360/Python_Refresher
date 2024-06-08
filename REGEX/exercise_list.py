3e87aa10707de2c647109ec69458a7c3a4ef419a
aganyo360 <wilsonaganyo@gmail.com>
2024-06-08 17:55:29 +0300

    added exercise.py

---
+++ REGEX/exercise_list.py
@@ -0,0 +1,13 @@
+invitees = ['Nyakiti', 'Elizabeth', 'Phillip', 'Dinna']
+message= 'Brice invites you, '
+print(message, invitees[0])
+print(message, invitees[1])
+print(message, invitees[2])
+print(message, invitees[3])
+
+print(f'unforutnaly {invitees.pop(-3)} will not be available')
+invitees.insert(-3, 'Bigsmoke')
+print(invitees)
+invitees.insert(-1, 'brice')
+print(invitees)
+print(f'The total number of invited guests are {len(invitees)}')
