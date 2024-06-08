5c8e359b567b3c0dd88bcdff5259f310d0db0cdb
aganyo360 <wilsonaganyo@gmail.com>
2024-06-08 17:59:54 +0300

    tupels in  py

---
+++ REGEX/tuple.py
@@ -0,0 +1,9 @@
+# a tuple is like a list but is immutable,it uses a parenthesis instead of a square brackets during definition
+measurements = (100,200)
+
+print(measurements[0])
+
+# while a list is mutable a tuple is immutable
+
+measurements[0]=23
+print(measurements[0])
