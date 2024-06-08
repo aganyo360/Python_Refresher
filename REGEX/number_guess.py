cfdd22645b4cfb7cbd2ea1a2a8300222fdf5d64b
aganyo360 <wilsonaganyo@gmail.com>
2024-06-08 18:01:25 +0300

    randomness in py

---
+++ REGEX/number_guess.py
@@ -0,0 +1,8 @@
+correct_answer = 7
+
+wild_guess = input('Pick a number between 0-9')
+
+if int(wild_guess) == correct_answer:
+    print('You guessed correctly!')
+else :
+    print('You guessed incorrectly!')
